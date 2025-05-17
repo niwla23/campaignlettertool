f267dc89b91168829a4baf38ebaad4680ff01da9
niwla23 <46248939+niwla23@users.noreply.github.com>
2025-05-17 18:06:59 +0200

    only run umami in prod

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	let isDev = import.meta.env.DEV;
 </script>
 
+<svelte:head>
+	{#if !isDev}
+		<script
+			defer
+			src="https://umami.cloudserver.click/homephoner"
+			data-website-id="58cd4073-5547-4afb-a07a-7d79e9fb91cb"
+		></script>
+	{/if}
+</svelte:head>
 {@render children()}
